a31c3cd2d08d362f6e4e1368928aa11783a57223
tblack10 <travis_black@live.com>
2021-03-09 16:45:07 -0800

    Added foreign key to ProgressUpdate Model

--- Web/Models/ProgressUpdate.cs
+++ Web/Models/ProgressUpdate.cs
@@ -6,10 +6,13 @@ namespace Web.Models {
     public class ProgressUpdate {
         [Key]
         public int Id { get; set; }
-        public Project Project { get; set; }
         public DateTime Date { get; set; }
         public string LastWeekActivity { get; set; }
         public string NextWeekActivity { get; set; }
         public string Issues { get; set; }
+        public int ProjectId { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; }
     }
 }
