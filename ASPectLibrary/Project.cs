7370a26c7173ccb7a815a18f929c08c482f18d24
John Vaganov <56142023+John9V@users.noreply.github.com>
2021-03-25 11:20:57 -0700

    Updated the project model

--- ASPectLibrary/Project.cs
+++ ASPectLibrary/Project.cs
@@ -1,7 +1,17 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace ASPectLibrary
 {
     public class Project
     {
+        [Key]
         public int ProjectId { get; set; }
+        public string  TeamName { get; set; }
+        public ProjectCategory ProjectCategory{ get; set; }
+        public string AppName { get; set; }
+        public string Description { get; set; }
+        public Guid AspNetUserId { get; set; }
+        public Course Course{ get; set; }
     }
 }
