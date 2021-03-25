f523da7b7a990cf12a78d98add9454ef7f4d0715
John Vaganov <56142023+John9V@users.noreply.github.com>
2021-03-25 11:28:47 -0700

    Improved project model

--- ASPectLibrary/Project.cs
+++ ASPectLibrary/Project.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ASPectLibrary
 {
@@ -8,10 +9,16 @@ namespace ASPectLibrary
         [Key]
         public int ProjectId { get; set; }
         public string  TeamName { get; set; }
-        public ProjectCategory ProjectCategory{ get; set; }
+        public int ProjectCategoryId { get; set; }
+
+        [ForeignKey("ProjectCategoryId")]
+        public ProjectCategory ProjectCategory { get; set; }
         public string AppName { get; set; }
         public string Description { get; set; }
         public Guid AspNetUserId { get; set; }
+        public int CourseId { get; set; }
+
+        [ForeignKey("CourseId")]
         public Course Course{ get; set; }
     }
 }
