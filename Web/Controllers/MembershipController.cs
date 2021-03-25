8dec41503ff926a974227b656df8137cffee71bf
Silooh <Danielhongjin@gmail.com>
2021-03-25 14:28:51 -0700

    Added membership controller.

---
+++ Web/Controllers/MembershipController.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASPectLibrary;
+using Web.Data;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembershipController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MembershipController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Membership
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Membership>>> GetMemberships()
+        {
+            return await _context.Memberships.ToListAsync();
+        }
+
+        // GET: api/Membership/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Membership>> GetMembership(string id)
+        {
+            var membership = await _context.Memberships.FindAsync(id);
+
+            if (membership == null)
+            {
+                return NotFound();
+            }
+
+            return membership;
+        }
+
+        // PUT: api/Membership/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMembership(string id, Membership membership)
+        {
+            if (id != membership.StudentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(membership).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MembershipExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Membership
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Membership>> PostMembership(Membership membership)
+        {
+            _context.Memberships.Add(membership);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (MembershipExists(membership.StudentId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetMembership", new { id = membership.StudentId }, membership);
+        }
+
+        // DELETE: api/Membership/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMembership(string id)
+        {
+            var membership = await _context.Memberships.FindAsync(id);
+            if (membership == null)
+            {
+                return NotFound();
+            }
+
+            _context.Memberships.Remove(membership);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MembershipExists(string id)
+        {
+            return _context.Memberships.Any(e => e.StudentId == id);
+        }
+    }
+}
--- Web/Data/ApplicationDbContext.cs
+++ Web/Data/ApplicationDbContext.cs
@@ -15,8 +15,8 @@ namespace Web.Data
         {
         }
         protected override void OnModelCreating (ModelBuilder builder) {
-            builder.Entity<Project> ().ToTable ("Project");
-
+            builder.Entity<Project>().ToTable ("Project");
+            builder.Entity<Membership>().HasOne(p => p.Project);
             base.OnModelCreating(builder);
         }
 
@@ -24,5 +24,7 @@ namespace Web.Data
         public DbSet<ProjectCategory> ProjectCategory { get; set; }
         public DbSet<ProgressUpdate> ProgressUpdates { get; set; }
         public DbSet<ASPectLibrary.Course> Course { get; set; }
+
+        public DbSet<Membership> Memberships { get; set; }
     }
 }
