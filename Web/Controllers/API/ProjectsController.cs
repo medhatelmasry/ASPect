f0174e7b7805b3884d6c128746dd7bbc9fbe1604
Alireza Kakan <alireza.kakan@gmail.com>
2021-03-25 11:54:44 -0700

    Created a folder that contains all the controllers.

---
+++ Web/Controllers/API/ProjectsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Data;
+
+namespace Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetAllProjects()
+        {
+            return null;
+        }
+    }
+}
