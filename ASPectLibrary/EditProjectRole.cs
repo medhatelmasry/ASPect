8aa2d0700181caad1ca393fa10f6c65c7b14fd1d
John Vaganov <56142023+John9V@users.noreply.github.com>
2021-03-25 11:39:53 -0700

    moved and adjusted files

--- ASPectLibrary/EditProjectRole.cs
+++ ASPectLibrary/EditProjectRole.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Web.Models
+namespace ASPetcLibrary
 {
     public class EditProjectRole
     {
--- ASPectLibrary/ProgressUpdate.cs
+++ ASPectLibrary/ProgressUpdate.cs
@@ -3,7 +3,8 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Web.Models {
+namespace ASPetcLibrary
+{
     public class ProgressUpdate {
         [Key]
         public int Id { get; set; }
