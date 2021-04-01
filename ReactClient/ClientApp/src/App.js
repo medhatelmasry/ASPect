90f26161a5528dbc25882e012b1ef913a1959894
Jaewhee Seo <jstylesss2@gmail.com>
2021-04-01 12:29:59 -0700

    Create Student Signup Page

--- ReactClient/ClientApp/src/App.js
+++ ReactClient/ClientApp/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { Route } from "react-router";
 import { Layout } from "./components/Layout";
 import { Home } from "./components/Home";
-import { FetchData } from "./components/FetchData";
-import { Counter } from "./components/Counter";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { Dashboard } from "./components/Dashboard";
 
 import "./custom.css";
 
@@ -15,9 +15,9 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path="/" component={Home} />
+        <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/login" component={Login} />
-        {/* <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} /> */}
+        <Route exact path="/signup" component={Signup} />
       </Layout>
     );
   }
