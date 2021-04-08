fda82d8818d6bdc7123fe6b48b23ae643ff5d35a
tblack10 <travis_black@live.com>
2021-04-08 11:47:14 -0700

    Created an assignment class, modified ProgressRequirement controller to find by AssignmentId, Modified ProjectRequirement Model to contain an Assignment

---
+++ ASPectLibrary/Assignment.cs
@@ -0,0 +1,33 @@
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPectLibrary { 
+    /// A class that is created and edited by an Instructor. References a Course.
+    /// A COURSE can have multiple ASSIGNMENTS, and an ASSIGNMENT can have multiple REQUIREMENTS.
+    public class Assignment {
+        /// The Assignment ID
+        [Key]
+        public int AssignmentId { get; set; }
+        
+        /// The date of creation of the assignment
+        public DateTime DateCreated { get; set; }
+
+        /// The expected due date of the assignment
+        public DateTime DueDate { get; set; }
+
+        /// A brief description of the assignment
+        public string description { get; set; }
+
+        /// The Id of the Course associated with the Assignment 
+        public int CourseId { get; set; }
+
+        /// The Course that has the Assignment
+        [ForeignKey("CourseId")]
+        public Course Course { get; set; }
+
+        public List<Assignment> Assignments { get; set; }
+    }
+}
