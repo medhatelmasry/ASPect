fff650c08b7e534605c7b7e287a7d6347255d969
yogiduzit <yverma2@my.bcit.ca>
2021-04-07 18:27:58 -0700

    Token auth working

--- Web/Controllers/AuthController.cs
+++ Web/Controllers/AuthController.cs
@@ -11,65 +11,63 @@ using Microsoft.IdentityModel.Tokens;
 
 public class AuthController : ControllerBase
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly IConfiguration _configuration;
+  private readonly UserManager<ApplicationUser> _userManager;
+  private readonly IConfiguration _configuration;
 
-    public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
-    {
-        _userManager = userManager;
-        _configuration = configuration;
-    }
+  public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+  {
+    _userManager = userManager;
+    _configuration = configuration;
+  }
 
-    [Route("api/[controller]")]
-    [HttpPost]
-    public async Task<ActionResult> InsertUser([FromBody] RegisterViewModel model)
+  [Route("api/[controller]/register")]
+  [HttpPost]
+  public async Task<ActionResult> InsertUser([FromBody] RegisterViewModel model)
+  {
+    var user = new ApplicationUser
     {
-        var user = new ApplicationUser
-        {
-            Email = model.Email,
-            UserName = model.Email,
-            FirstName = model.FirstName,
-            LastName = model.LastName,
-            SecurityStamp = Guid.NewGuid().ToString()
-        };
-        var result = await _userManager.CreateAsync(user, model.Password);
-        // if (result.Succeeded) {
-        //   await _userManager.AddToRoleAsync(user, "Customer");
-        // }
-        return Ok(new { Username = user.UserName });
-    }
+      Email = model.Email,
+      UserName = model.Email,
+      FirstName = model.FirstName,
+      LastName = model.LastName,
+      SecurityStamp = Guid.NewGuid().ToString()
+    };
+    var result = await _userManager.CreateAsync(user, model.Password);
+    // if (result.Succeeded) {
+    //   await _userManager.AddToRoleAsync(user, "Customer");
+    // }
+    return Ok(new { Username = user.UserName });
+  }
 
-    [Route("login")]
-    [HttpPost]
-    public async Task<ActionResult> Login([FromBody] LoginViewModel model)
+  [Route("api/[controller]/login")]
+  [HttpPost]
+  public async Task<ActionResult> Login([FromBody] LoginViewModel model)
+  {
+    var user = await _userManager.FindByEmailAsync(model.Username);
+    if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
     {
-        var user = await _userManager.FindByNameAsync(model.Username);
-        if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
-        {
-            var claim = new[] {
+      var claim = new[] {
         new Claim(JwtRegisteredClaimNames.Sub, user.UserName)
       };
-            var signinKey = new SymmetricSecurityKey(
-              Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
+      var signinKey = new SymmetricSecurityKey(
+        Encoding.UTF8.GetBytes(_configuration["Jwt:SigningKey"]));
 
-            int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
+      int expiryInMinutes = Convert.ToInt32(_configuration["Jwt:ExpiryInMinutes"]);
 
-            var token = new JwtSecurityToken(
-              issuer: _configuration["Jwt:Site"],
-              audience: _configuration["Jwt:Site"],
-              expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
-              signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
-            );
+      var token = new JwtSecurityToken(
+        issuer: _configuration["Jwt:Site"],
+        audience: _configuration["Jwt:Site"],
+        expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+        signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+      );
 
-            return Ok(
-              new
-              {
-                  token = new JwtSecurityTokenHandler().WriteToken(token),
-                  expiration = token.ValidTo
-              });
-        }
-        return Unauthorized();
+      return Ok(
+        new
+        {
+          token = new JwtSecurityTokenHandler().WriteToken(token),
+          expiration = token.ValidTo
+        });
     }
-}
-
+    return Unauthorized();
+  }
 }
