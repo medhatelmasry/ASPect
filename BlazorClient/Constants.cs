843be3cfde7be6162dbc3fd6b69af92e8cb6f970
John Vaganov <56142023+John9V@users.noreply.github.com>
2021-04-08 16:45:30 -0700

    progress on correct api link

--- BlazorClient/Constants.cs
+++ BlazorClient/Constants.cs
@@ -1,11 +1,17 @@
+using System;
+
 namespace BlazorClient
 {
     public class Constants
     {
         public partial class API
         {
-            public const string DEV_URL = "https://localhost:5001/api";
-            public const string PRODUCTION_URL = "";
+            public static string DEV_URL = "https://localhost:5001/api";
+            public static string PRODUCTION_URL = "https://openaspect.azurewebsites.net/api";
+
+            public static bool IS_DEVELOPMENT = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+
+            public static string API_URL = IS_DEVELOPMENT ? DEV_URL : PRODUCTION_URL;
         }
     }
 }
