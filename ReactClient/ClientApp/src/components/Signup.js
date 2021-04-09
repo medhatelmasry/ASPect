57c52b65770fccc95222f5957a0f8c04a2617689
Jaewhee Seo <jstylesss2@gmail.com>
2021-04-08 17:21:14 -0700

    Connect Student Sign Up View with Database
    - Add input validation features (e.g., only letters into name fields, checking existing emails, etc)
    
    - Students can create accounts into database successfully

--- ReactClient/ClientApp/src/components/Signup.js
+++ ReactClient/ClientApp/src/components/Signup.js
@@ -5,12 +5,24 @@ import { useFormik, FormikProvider, Form } from "formik";
 import { Button, Alert, Row } from "react-bootstrap";
 import FormContainer from "./common/FormContainer";
 import TextInputLiveFeedback from "./common/TextInputLiveFeedback";
-import { emailRegex, passwordRegex } from "../util/regex";
+import { nameRegex, emailRegex, passwordRegex } from "../util/regex";
+import bcrypt from "bcryptjs";
+import axios from "axios";
 
 const schema = Yup.object({
   email: Yup.string()
     .required("Email is required")
     .matches(emailRegex, "Invalid"),
+  firstname: Yup.string()
+    .min(2, "Must be at least 2 characters")
+    .max(20, "Must be less than 20 characters")
+    .required("First name is required")
+    .matches(nameRegex, "Invalid"),
+  lastname: Yup.string()
+    .min(2, "Must be at least 2 characters")
+    .max(20, "Must be less than 20 characters")
+    .required("Last name is required")
+    .matches(nameRegex, "Invalid"),
   password: Yup.string()
     .min(8, "Must be at least 8 characters")
     .max(20, "Must be less than 20 characters")
@@ -27,6 +39,14 @@ const schema = Yup.object({
     }),
 });
 
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+};
+
 const Signup = ({ onSubmit }) => {
   const [error, setError] = useState("");
   const [showAlert, setShowAlert] = useState(false);
@@ -35,20 +55,49 @@ const Signup = ({ onSubmit }) => {
   const formik = useFormik({
     initialValues: {
       email: "",
+      userName: "",
+      normalizedUserName: "",
+      normalizedEmail: "",
+      firstname: "",
+      lastname: "",
       password: "",
       confirmPassword: "",
+      emailConfirmed: true,
+      passwordHash: "",
     },
     onSubmit: async (values) => {
+      values.userName = values.email;
+      values.firstname =
+        values.firstname.charAt(0).toUpperCase() + values.firstname.slice(1);
+      values.lastname =
+        values.lastname.charAt(0).toUpperCase() + values.lastname.slice(1);
+      values.normalizedUserName = values.email.toUpperCase();
+      values.normalizedEmail = values.email.toUpperCase();
+      const passwordHash = await bcrypt.hash(values.password, 10);
+      values.passwordHash = passwordHash;
       console.log(values);
 
-      // if (val.length === 0) {
-      history.push("/dashboard");
-      // } else {
-      //   setError("Email is already in use");
-      //   setShowAlert(true);
-      //   console.log(error);
-      //   history.push("/signup");
-      // }
+      const result = await axios.get("/api/Student", config);
+      const studentList = result.data;
+      let studentUserNameList = [];
+
+      studentList.forEach((student) => {
+        studentUserNameList.push(student.userName);
+      });
+
+      if (studentUserNameList.includes(values.userName)) {
+        setError("Email is already in use");
+        setShowAlert(true);
+        history.push("/signup");
+      } else {
+        try {
+          await axios.post("/api/Student", values, config);
+          history.push("/dashboard");
+          console.log("created a student account");
+        } catch (error) {
+          console.log(error);
+        }
+      }
     },
     validationSchema: schema,
   });
@@ -79,6 +128,20 @@ const Signup = ({ onSubmit }) => {
           />
 
           <TextInputLiveFeedback
+            label="First Name"
+            id="firstname"
+            name="firstname"
+            type="text"
+          />
+
+          <TextInputLiveFeedback
+            label="Last Name"
+            id="lastname"
+            name="lastname"
+            type="text"
+          />
+
+          <TextInputLiveFeedback
             label="Password"
             id="password"
             name="password"
@@ -91,6 +154,14 @@ const Signup = ({ onSubmit }) => {
             name="confirmPassword"
             type="password"
           />
+
+          <Row className="text-center">
+            <p className="mx-4 text-left" style={{ fontSize: "0.75rem" }}>
+              Password must conatin 8-20 characters, at least 1 numeric
+              character, at least 1 lowercase letter, at least 1 uppercase
+              letter, and at least 1 special character.
+            </p>
+          </Row>
 
           <Button
             type="submit"
